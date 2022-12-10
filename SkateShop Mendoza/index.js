100c07e896758a41519b9f7bd3b8b6f510bbd3dd
miguelmendozan <107602636+miguelmendozan@users.noreply.github.com>
2022-12-10 02:16:33 -0500

    Correcion #1

--- SkateShop Mendoza/index.js
+++ SkateShop Mendoza/index.js
@@ -3,11 +3,14 @@
 
 const divProductos = document.querySelector("#divProductos");
 const divTotal = document.querySelector("#divTotal");
+
+// Consumiendo Json de productos
 fetch("productos.json")
-.then(respuesta => respuesta.json())
-.then( productos =>{
-  productos.forEach((producto) => {
-    divProductos.innerHTML += `
+  .then((respuesta) => respuesta.json())
+  .then((productos) => {
+    //renderizamos los productos
+    productos.forEach((producto) => {
+      divProductos.innerHTML += `
       <div id="${producto.id}" class="card cardProducto">
       <div class="card-body">
       <h5 class="card-title">${producto.nombre}</h5>
@@ -17,47 +20,51 @@ fetch("productos.json")
       </div>
       </div>
       `;
-})})
+    });
+    //carrito de compra
 
+    const carrito = [];
+    const botonesAgregar = document.querySelectorAll(".btn-warning");
 
-//carrito de compra
-const carrito = [];
-const botonesAgregar = document.querySelectorAll(".btn-warning");
+    botonesAgregar.forEach((boton) => {
+      boton.onclick = () => {
+        const producto = productos.find(
+          (prod) => prod.id === parseInt(boton.id)
+        );
+        const productoCarrito = {
+          id: producto.id,
+          nombre: producto.nombre,
+          precio: producto.precio,
+          cantidad: 1,
+        };
+        const indexCarrito = carrito.findIndex(
+          (prod) => prod.id === producto.id
+        );
+        if (indexCarrito === -1) {
+          carrito.push(productoCarrito);
+        } else {
+          carrito[indexCarrito].cantidad += 1;
+        }
+        guardarCarritoLocalStorage();
+      };
+    });
 
-botonesAgregar.forEach((boton) => {
-  boton.onclick = () => {
-    const producto = productosArray.find(
-      (prod) => prod.id === parseInt(boton.id)
-    );
-    const productoCarrito = {
-      id: producto.id,
-      nombre: producto.nombre,
-      precio: producto.precio,
-      cantidad: 1,
+    const botonFinalizar = document.querySelector("#finalizar");
+    botonFinalizar.onclick = () => {
+      const totalCompra = carrito
+        .map((prod) => prod.precio * prod.cantidad)
+        .reduce((elem1, elem2) => elem1 + elem2);
+
+      swal(
+        "Gracias por tu compra!",
+        "El total de tu compra es " + totalCompra,
+        "success"
+      );
     };
-    const indexCarrito = carrito.findIndex((prod) => prod.id === producto.id);
-    if (indexCarrito === -1) {
-      carrito.push(productoCarrito);
-    } else {
-      carrito[indexCarrito].cantidad += 1;
-    }
-    guardarCarritoLocalStorage();
-  };
-});
+  });
 
-const botonFinalizar = document.querySelector("#finalizar");
-botonFinalizar.onclick = () => {
-  const totalCompra = carrito
-    .map((prod) => prod.precio * prod.cantidad)
-    .reduce((elem1, elem2) => elem1 + elem2);
-
-  swal(
-    "Gracias por tu compra!",
-    "El total de tu compra es " + totalCompra,
-    "success"
-  );
-};
-
+// guardar carrito local storage
 function guardarCarritoLocalStorage() {
   localStorage.setItem("carrito", JSON.stringify(carrito));
 }
+
